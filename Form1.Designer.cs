1ac5fcfef553ab48d46b0431b715cb410b6fdfd0
Amit Lakade <99075733+lakadeamit220@users.noreply.github.com>
2024-09-07 17:27:06 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace ProductManagerWinForms
+{
+    partial class ProductManagerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtProductName = new System.Windows.Forms.TextBox();
+            this.txtCategory = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.dataGridViewProducts = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblSearchCategory = new System.Windows.Forms.Label();
+            this.txtSearchCategory = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProducts)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtProductName
+            // 
+            this.txtProductName.Location = new System.Drawing.Point(250, 16);
+            this.txtProductName.Name = "txtProductName";
+            this.txtProductName.Size = new System.Drawing.Size(186, 22);
+            this.txtProductName.TabIndex = 0;
+            // 
+            // txtCategory
+            // 
+            this.txtCategory.Location = new System.Drawing.Point(250, 63);
+            this.txtCategory.Name = "txtCategory";
+            this.txtCategory.Size = new System.Drawing.Size(186, 22);
+            this.txtCategory.TabIndex = 1;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(250, 107);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(186, 22);
+            this.txtPrice.TabIndex = 2;
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Location = new System.Drawing.Point(250, 152);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(186, 22);
+            this.txtQuantity.TabIndex = 3;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnInsert.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInsert.Location = new System.Drawing.Point(28, 217);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(98, 43);
+            this.btnInsert.TabIndex = 4;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = false;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.btnUpdate.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(142, 217);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(102, 43);
+            this.btnUpdate.TabIndex = 5;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.IndianRed;
+            this.btnDelete.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(381, 217);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(85, 43);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.BackColor = System.Drawing.Color.SeaGreen;
+            this.btnDisplay.Font = new System.Drawing.Font("Arial Narrow", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDisplay.Location = new System.Drawing.Point(260, 217);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(100, 43);
+            this.btnDisplay.TabIndex = 7;
+            this.btnDisplay.Text = "Display";
+            this.btnDisplay.UseVisualStyleBackColor = false;
+            this.btnDisplay.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // dataGridViewProducts
+            // 
+            this.dataGridViewProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewProducts.Location = new System.Drawing.Point(472, 12);
+            this.dataGridViewProducts.Name = "dataGridViewProducts";
+            this.dataGridViewProducts.RowHeadersWidth = 51;
+            this.dataGridViewProducts.RowTemplate.Height = 24;
+            this.dataGridViewProducts.Size = new System.Drawing.Size(962, 522);
+            this.dataGridViewProducts.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(43, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 23);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "ProductName";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(43, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 23);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Category";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(43, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 23);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Price";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(43, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 23);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Quantity";
+            // 
+            // lblSearchCategory
+            // 
+            this.lblSearchCategory.AutoSize = true;
+            this.lblSearchCategory.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblSearchCategory.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSearchCategory.Location = new System.Drawing.Point(24, 305);
+            this.lblSearchCategory.Name = "lblSearchCategory";
+            this.lblSearchCategory.Size = new System.Drawing.Size(176, 23);
+            this.lblSearchCategory.TabIndex = 13;
+            this.lblSearchCategory.Text = "SearchByCategory";
+            // 
+            // txtSearchCategory
+            // 
+            this.txtSearchCategory.Location = new System.Drawing.Point(250, 306);
+            this.txtSearchCategory.Name = "txtSearchCategory";
+            this.txtSearchCategory.Size = new System.Drawing.Size(186, 22);
+            this.txtSearchCategory.TabIndex = 14;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.DarkCyan;
+            this.btnSearch.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(28, 373);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(114, 43);
+            this.btnSearch.TabIndex = 15;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.Orange;
+            this.btnReset.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(260, 373);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(98, 43);
+            this.btnReset.TabIndex = 16;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // ProductManagerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(1437, 561);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearchCategory);
+            this.Controls.Add(this.lblSearchCategory);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridViewProducts);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtCategory);
+            this.Controls.Add(this.txtProductName);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "ProductManagerForm";
+            this.Text = "ProductManagerForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProducts)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtProductName;
+        private System.Windows.Forms.TextBox txtCategory;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.DataGridView dataGridViewProducts;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblSearchCategory;
+        private System.Windows.Forms.TextBox txtSearchCategory;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace ProductManagerWinForms
+{
+    public partial class ProductManagerForm : Form
+    {
+        // Connection string to connect to the SQL Server database
+        private static string connectionString = "Server=AMITLAKADE\\SQLEXPRESS;Database=sqldb;Trusted_Connection=True;";
+
+        public ProductManagerForm()
+        {
+            InitializeComponent();
+            LoadData(); // Load data when the form is initialized
+        }
+
+        // Event handler for the Insert button
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            // Retrieve input values from text boxes
+            string productName = txtProductName.Text;
+            string category = txtCategory.Text;
+            decimal price;
+            int quantity;
+
+            // Validate and parse the price
+            if (!decimal.TryParse(txtPrice.Text, out price))
+            {
+                MessageBox.Show("Please enter a valid price.");
+                return; // Stop execution if the price is invalid
+            }
+
+            // Validate and parse the quantity
+            if (!int.TryParse(txtQuantity.Text, out quantity))
+            {
+                MessageBox.Show("Please enter a valid quantity.");
+                return; // Stop execution if the quantity is invalid
+            }
+
+            // Insert the record into the database
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "INSERT INTO Products (ProductName, Category, Price, Quantity) VALUES (@ProductName, @Category, @Price, @Quantity)";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    // Add parameters to avoid SQL injection
+                    command.Parameters.AddWithValue("@ProductName", productName);
+                    command.Parameters.AddWithValue("@Category", category);
+                    command.Parameters.AddWithValue("@Price", price);
+                    command.Parameters.AddWithValue("@Quantity", quantity);
+
+                    try
+                    {
+                        connection.Open(); // Open the connection
+                        command.ExecuteNonQuery(); // Execute the insert command
+                        MessageBox.Show("Record inserted successfully!");
+                    }
+                    catch (Exception ex)
+                    {
+                        // Handle exceptions
+                        MessageBox.Show("An error occurred: " + ex.Message);
+                    }
+                }
+            }
+
+            // Clear text fields and reload data to reflect changes
+            ClearTextFields();
+            LoadData();
+        }
+
+        // Event handler for the Update button
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int productId;
+            string productName = txtProductName.Text;
+            string category = txtCategory.Text;
+            decimal price;
+            int quantity;
+
+            // Validate ProductID from selected row in DataGridView
+            if (dataGridViewProducts.CurrentRow == null || !int.TryParse(dataGridViewProducts.CurrentRow.Cells[0].Value.ToString(), out productId))
+            {
+                MessageBox.Show("Invalid Product ID.");
+                return;
+            }
+
+            // Validate Price
+            if (!decimal.TryParse(txtPrice.Text, out price))
+            {
+                MessageBox.Show("Invalid price format. Please enter a valid decimal number.");
+                return;
+            }
+
+            // Validate Quantity
+            if (!int.TryParse(txtQuantity.Text, out quantity))
+            {
+                MessageBox.Show("Invalid quantity format. Please enter a valid integer.");
+                return;
+            }
+
+            // Update the record in the database
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "UPDATE Products SET ProductName = @ProductName, Category = @Category, Price = @Price, Quantity = @Quantity WHERE ProductID = @ProductID";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    // Add parameters to avoid SQL injection
+                    command.Parameters.AddWithValue("@ProductID", productId);
+                    command.Parameters.AddWithValue("@ProductName", productName);
+                    command.Parameters.AddWithValue("@Category", category);
+                    command.Parameters.AddWithValue("@Price", price);
+                    command.Parameters.AddWithValue("@Quantity", quantity);
+
+                    try
+                    {
+                        connection.Open(); // Open the connection
+                        command.ExecuteNonQuery(); // Execute the update command
+                        MessageBox.Show("Record updated successfully!");
+                    }
+                    catch (Exception ex)
+                    {
+                        // Handle exceptions
+                        MessageBox.Show($"Error updating record: {ex.Message}");
+                    }
+                }
+            }
+
+            // Clear text fields and reload data to reflect changes
+            ClearTextFields();
+            LoadData();
+        }
+
+        // Event handler for the Delete button
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int productId;
+
+            // Validate selected product from DataGridView
+            if (dataGridViewProducts.CurrentRow == null || !int.TryParse(dataGridViewProducts.CurrentRow.Cells[0].Value.ToString(), out productId))
+            {
+                MessageBox.Show("Select a record to delete.");
+                return;
+            }
+
+            // Delete the record from the database
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "DELETE FROM Products WHERE ProductID = @ProductID";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    // Add parameter to avoid SQL injection
+                    command.Parameters.AddWithValue("@ProductID", productId);
+
+                    try
+                    {
+                        connection.Open(); // Open the connection
+                        command.ExecuteNonQuery(); // Execute the delete command
+                        MessageBox.Show("Record deleted successfully!");
+                    }
+                    catch (Exception ex)
+                    {
+                        // Handle exceptions
+                        MessageBox.Show($"Error deleting record: {ex.Message}");
+                    }
+                }
+            }
+
+            // Clear text fields and reload data to reflect changes
+            ClearTextFields();
+            LoadData();
+        }
+
+        // Event handler for the Load button
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            LoadData(); // Reload data from the database
+        }
+
+        // Method to load data from the database into the DataGridView
+        private void LoadData()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM Products";
+                using (SqlDataAdapter adapter = new SqlDataAdapter(query, connection))
+                {
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable); // Fill DataTable with data from the database
+                    dataGridViewProducts.DataSource = dataTable; // Bind DataTable to DataGridView
+                }
+            }
+        }
+
+        // Method to clear all text fields
+        private void ClearTextFields()
+        {
+            txtProductName.Clear();
+            txtCategory.Clear();
+            txtPrice.Clear();
+            txtQuantity.Clear();
+        }
+
+        private void SearchByCategory(string category)
+        {
+            // Define the query to select ProductName and Price based on category
+            //string query = "SELECT ProductName, Price FROM Products WHERE Category = @Category";
+            string query = "SELECT ProductName, Price FROM Products WHERE LOWER(Category) = LOWER(@Category)";
+
+
+            // Use a DataTable to store the results
+            DataTable searchResults = new DataTable();
+
+            // Establish a connection using the connection string
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    // Create a SqlDataAdapter with the query and connection
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(query, connection))
+                    {
+                        // Add the @Category parameter to prevent SQL injection
+                        adapter.SelectCommand.Parameters.AddWithValue("@Category", category);
+
+                        // Fill the DataTable with the search results
+                        adapter.Fill(searchResults);
+                    }
+
+                    //Provide user feedback when no records match the search criteria
+                    if (searchResults.Rows.Count == 0)
+                    {
+                        MessageBox.Show("No products found in the specified category.");
+                    }
+
+                    // Bind the search results to the DataGridView
+                    dataGridViewProducts.DataSource = searchResults;
+
+                    // Optional: Adjust DataGridView columns (e.g., set headers)
+                    dataGridViewProducts.Columns["ProductName"].HeaderText = "Product Name";
+                    dataGridViewProducts.Columns["Price"].HeaderText = "Price";
+                }
+                catch (Exception ex)
+                {
+                    // Handle any errors that occur during the search
+                    MessageBox.Show("An error occurred while searching: " + ex.Message);
+                }
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            // Retrieve the category entered by the user
+            string category = txtSearchCategory.Text.Trim();
+
+            // Validate the input
+            if (string.IsNullOrEmpty(category))
+            {
+                MessageBox.Show("Please enter a category to search.");
+                return;
+            }
+
+            // Call the search method
+            SearchByCategory(category);
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            // Clear the search TextBox
+            txtSearchCategory.Clear();
+
+            // Reload all data
+            LoadData();
+        }
+    }
+}
